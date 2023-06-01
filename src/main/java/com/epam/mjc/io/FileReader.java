162525dcd3052312c38e4eca905b68c620293d37
Ayan <ajan.kohan@gmail.com>
2023-06-01 18:09:49 +0600

    IO task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,30 +1,28 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file){
-        String string ="";
+    public Profile getDataFromFile(File file) {
+        String string = "";
         java.io.FileReader reader = null;
-        try{
+        try {
             reader = new java.io.FileReader(file.getAbsoluteFile());
             int data = reader.read();
-            while(data !=-1){
-                string +=(char) data;
+            while (data != -1) {
+                string += (char) data;
                 data = reader.read();
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            try{
-                reader.close();
-            }
-            catch (IOException e){
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -32,10 +30,10 @@ public class FileReader {
 
         String[] values = new String[4];
         int j = 0;
-        for(String i : string.split("\n")){
-            int start = i.indexOf(": ")+1;
+        for (String i : string.split("\n")) {
+            int start = i.indexOf(": ") + 1;
             String value = i.substring(start).trim();
-            values[j]= value;
+            values[j] = value;
             j++;
         }
         String name = values[0];
@@ -43,7 +41,7 @@ public class FileReader {
         String email = values[2];
         Long phone = Long.valueOf(values[3]);
 
-        return new Profile(name,age,email,phone);
+        return new Profile(name, age, email, phone);
     }
 
 }
